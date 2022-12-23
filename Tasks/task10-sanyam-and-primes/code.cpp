4001ab1645197dbaa031e497977b4383bb32e1c1
Ribhav Bansal <96466752+RibhavBansal@users.noreply.github.com>
2022-12-23 21:32:11 +0530

    Update code.cpp

--- Tasks/task10-sanyam-and-primes/code.cpp
+++ Tasks/task10-sanyam-and-primes/code.cpp
@@ -3,24 +3,26 @@ using namespace std;
 #define int long long
 const int mod = 998244353;
 const int N = 2e5;
+int mx = 0;
 
-void primeFactors(int n, vector<int>&v) 
+void primeFactors(int n) 
 { 
     while (n%2 == 0) 
     { 
-        v.push_back(2); 
+        // v.push_back(2);
+        mx = 2;
         n = n/2; 
     } 
     for (int i = 3; i <= sqrt(n); i = i+2) 
     { 
         while (n%i == 0) 
         { 
-            v.push_back(i);
+            mx = max(mx,i);
             n = n/i; 
         } 
     } 
     if (n > 2) 
-        v.push_back(n); 
+        mx = max(mx,n);
 } 
 
 int32_t main(){
@@ -28,14 +30,9 @@ int32_t main(){
     cin >> t;
     while(t--){
     int n; cin>>n;
-    vector<int> v;
-    primeFactors(n,v);
-    sort(v.begin(),v.end());
-    reverse(v.begin(),v.end());
-    int k = 1;
-    
-    k = v[0];
-    cout << n/k << endl;
+    // vector<int> v;
+    primeFactors(n);
+    cout << n/mx << endl;
     }
 }
 
