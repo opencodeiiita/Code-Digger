9f6734caacd4c8b2260958884ab6893e6c0579ee
Sanyam07 <agrawalsanyam07@gmail.com>
2022-12-25 13:34:02 +0530

    Task-11 Solution

--- Tasks/task11-Safe-Distance/code.cpp
+++ Tasks/task11-Safe-Distance/code.cpp
@@ -14,31 +14,6 @@ typedef vector<bool> vb;
 #define mod 1000000007
 #define mod1 998244353
 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-
 bool cmpy(pair<ll,ll> a,pair<ll,ll> b){
 	if(a.ss!=b.ss) return a.ss<b.ss;
 	return a.ff<b.ff;
@@ -69,8 +44,16 @@ void solve(){
 		if(girls[i].ff<leftgirl) leftgirl=girls[i].ff;
 		if(girls[i].ss<downgirl) downgirl=girls[i].ss;
 	}
+	if(rightboy<leftgirl){
+		cout<<"0";
+		return;
+	}
 	ll ans=rightboy-leftgirl;
 	if(upboy>=downgirl) ans++;
+	if(upboy<downgirl){
+		cout<<"0";
+		return;
+	}
 	ll temp=upboy-downgirl;
 	if(rightboy>=leftgirl) temp++;
 	if(temp<ans) ans=temp;
@@ -101,7 +84,8 @@ void solve(){
 				break;
 			}
 		}
-		moves2+=(boys[indb].ss-girls[ind].ss+1);
+		if(boys[indb].ss>=girls[ind].ss)
+			moves2+=(boys[indb].ss-girls[ind].ss+1);
 		if(moves2<ans) ans=moves2;
 		moves+=(rightboy-girls[ind].ff+1);
 		moved.ff-=(rightboy-girls[ind].ff+1);
@@ -115,9 +99,6 @@ int main(){
    ios_base::sync_with_stdio(0);
    cin.tie(0);
    cout.tie(0);
-   #ifndef ONLINE_JUDGE
-      freopen("Error.txt", "w", stderr);
-   #endif
 
    solve();
    return 0;
