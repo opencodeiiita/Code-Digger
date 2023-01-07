cda75d35d9ae4163428e60ea28c3b1ed7dd49162
Ansh Kothari <kothariansh31@gmail.com>
2023-01-07 21:47:13 +0530

    Update Ansh_Kothari.cpp

--- Tasks/task17-find-fibonacci-number/Ansh_Kothari.cpp
+++ Tasks/task17-find-fibonacci-number/Ansh_Kothari.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int fibonacci(int n, vector<int>&dp) {
-    if(n==0) return 0;
-    if(n==1) return 1;
+    if(n==1) return 0;
+    if(n==2) return 1;
 
     if(dp[n]!=-1) return dp[n];
 
